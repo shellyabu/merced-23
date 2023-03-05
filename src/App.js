13fd2e238d64170dfa3bb78d7c752515d506bcf1
Robert Palermo <palermor@Roberts-MacBook-Pro.local>
2023-03-04 18:49:05 -0800

    src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>wecedMerced!</h1>
+
+      <div className="form">
+        <label>Events: </label>
+        <input type="text" name="Event" />
+        <label>Summary: </label>
+        <input type="text" name="Summary" />
+
+        <Button className="Button">
+          Submit
+        </Button>
+        
+      </div>
+      
     </div>
   );
 }
