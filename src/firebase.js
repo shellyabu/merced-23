62f81ec7df4f2cbe45ae80bb549be88af91dbe07
shellyabu <abu.shelly@gmail.com>
2023-03-05 01:14:02 -0800

    auth working email + making user, comment sect added

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,10 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "@firebase/firestore";
+import { getAuth,createUserWithEmailAndPassword, updateProfile, onAuthStateChanged, signOut, signInWithEmailAndPassword,  } from 'firebase/auth';
+import { addDoc, collection, onSnapshot, query, orderBy } from 'firebase/firestore';
+
+import{
+  getFirestore,
+ } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCEAqWutvG4Rh3fEUSk7Y0q1CLmoZCKFtA",
@@ -12,4 +17,51 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+//==== FIREBASE AUTH ====
+export function createAccount(username, email, password) {
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredentials) => {
+    updateProfile(userCredentials.user, { displayName: username });
+  })
+  .catch((error) => {
+    alert(error);
+  });
+}
+
+export function signInUser(email, password) {
+  signInWithEmailAndPassword(auth, email, password).catch((error)=>{
+    alert(error);
+  })
+}
+
+export function signOutUser() {
+  signOut(auth).catch((error) => {
+    alert(error);
+  });
+}
+
+export function updateOnAuthStateChanged(callback) {
+  onAuthStateChanged(auth, callback);
+}
+
+//===== Comment =======
+export function dbAddComment(userID, username, comment) {
+  if (comment.trim() !== '') {
+    addDoc(collection(db, 'comments'), {
+      comment: comment,
+      timestamp: Date.now(),
+      userID: userID,
+      username: username,
+    }).catch((error) => {
+      alert(error);
+    });
+  }
+}
+
+export function updateOnSnapshot(callback) {
+  const q = query(collection(db, 'comments'), orderBy('timestamp', 'desc'));
+	onSnapshot(q, callback)
+}
--- src/home.jsx
+++ src/home.jsx
@@ -1,40 +1,176 @@
-import React, { useRef } from 'react';
-import { firestore } from "./firebase";
-import { addDoc, collection } from "@firebase/firestore"
-import {Button} from '@mui/material';
+import {
+  createAccount,
+  updateOnAuthStateChanged,
+  signOutUser,
+  signInUser,
+  dbAddComment,
+  updateOnSnapshot,
+} from './firebase.js';
+import './App.css';
+import {
+  TextField,
+  Typography,
+  Stack,
+  Divider,
+  Button,
+  Card,
+  CardContent,
+  Input,
+  InputAdornment,
+  OutlinedInput,
+  IconButton,
+} from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 
-export default function () {
-    const messageRef = useRef();
-    const ref = collection(firestore, "messages");
+function signUpUser(email, password) {
+  const username = prompt('insert username');
+  if (username) {
+    createAccount(username, email, password);
+  } else {
+    alert('no username entered');
+  }
+}
 
+//account stuff
+function AccountComponent(user) {
+  if (user) {
+    return (
+      <Stack direction="column" alignItems="flex-start">
+        <Typography gutterBottom variant="body1">
+          signed in as <b>{user.displayName}</b>
+        </Typography>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            signOutUser();
+          }}
+        >
+          Sign Out
+        </Button>
+      </Stack>
+    );
+  } else {
+    return <InForm />;
+  }
+}
 
-    const handleSave = async(e) => {
-        e.preventDefault();
-        console.log(messageRef.current.value);
+function InForm() {
+  const email_ref = useRef('');
+  const password_ref = useRef('');
+  return (
+    <Stack direction="column" spacing={2}>
+      <Input inputRef={email_ref} id="email-input" placeholder="email" />
+      <Input
+        inputRef={password_ref}
+        id="password-input"
+        placeholder="password"
+        type="password"
+      />
 
-        let data = {
-            message:messageRef.current.value,
-        }
+      <Stack direction="row" justifyContent="space-around">
+        <Button
+          variant="outlined"
+          onClick={() => {
+            signInUser(email_ref.current.value, password_ref.current.value);
+          }}
+        >
+          login
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            signUpUser(email_ref.current.value, password_ref.current.value);
+          }}
+        >
+          sign up
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
 
-        try {
-            addDoc(ref,data)
-        }catch(e){
-            console.log(e);
-        }
-    };
+//comment stuff
+function CommentsSection({ user }) {
+  const comment_ref = useRef([]);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    updateOnSnapshot((snapshot) => {
+      const comments = [];
+      snapshot.forEach((document) => {
+        const docData = document.data();
+        comments.push(
+          <Comment username={docData.username} content={docData.comment} />
+        );
+      });
+      setComments(comments);
+    });
+  }, []);
 
   return (
-    <div>
-        <form onSubmit={handleSave}>
-            <label>Business Name: </label>
-            <input type="text" placeholder='Business Name' ref={messageRef} />
-            <label>Password: </label>
-            <input type="password" placeholder='Password' ref={messageRef} />
+    <Stack direction="column" spacing={2}>
+      <Typography gutterBottom variant="h4">
+        Comments
+      </Typography>
 
-            <Button type="submit">
-                Submit
-            </Button>
-        </form>
-    </div>
-  )
+      <OutlinedInput
+        multiline
+        inputRef={comment_ref}
+        disabled={user ? false : true}
+        id="comment-field"
+        placeholder="comment here..."
+        endAdornment={
+          <InputAdornment
+            position="end"
+            sx={{ position: 'absolute', bottom: 20, right: 5 }}
+          >
+            <IconButton
+              onClick={() => {
+                dbAddComment(
+                  user.uid,
+                  user.displayName,
+                  comment_ref.current.value
+                );
+                comment_ref.current.value = '';
+              }}
+              disabled={user ? false : true}
+            >
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+      <Stack direction="column" spacing={2}>
+        {comments}
+      </Stack>
+    </Stack>
+  );
+}
+
+function Comment({ username, content }) {
+  return (
+    <Card className="comment-bg">
+      <CardContent>
+        <Typography gutterBottom variant="h6" component="div">
+          {username}
+        </Typography>
+        <Typography variant="body2">{content}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default function App() {
+  const [user, setUser] = useState();
+  updateOnAuthStateChanged((userIn) => {
+    setUser(userIn);
+  });
+
+  return (
+    <Stack direction="column" spacing={3}>
+      <Typography variant="h4">Account</Typography>
+      {AccountComponent(user)}
+      <Divider />
+      <CommentsSection user={user} />
+    </Stack>
+  );
 }
